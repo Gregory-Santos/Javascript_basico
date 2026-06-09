a8638fe9af67f090983defbcd16baaf91a80946f
Gregory <gregorysantos575@gmail.com>
2026-06-09 16:38:27 -0300

    . commit

---
+++ Soluções Exercícios/aula04-pg/js/desafio.js
@@ -0,0 +1,39 @@
+/*
+let idade = Number(prompt("Informe a sua idade."))
+
+if(idade >=18) {
+    console.log("Você é maior de idade.")
+} else {
+    console.log("Você é menor de idade.")
+}
+
+/*let umAtres = prompt("Informe um número de 1 a 3."
+
+switch(umAtres) {
+    case "1" : alert("Baixo.")
+    break;
+    case "2" : alert("Médio")
+    break;
+    case "3" : alert("Alto")
+    break;
+
+    default: alert("Sem definição.")
+}*/
+
+/*let num = Number(prompt("Escolha um número."))
+
+if (num > 0) {
+    alert("É um número positivo!")
+} else {
+    alert("É um número negativo.")
+}*/
+
+/*let idade16 = Number(prompt("Qual a sua idade?")) >= 16;
+ 
+let nacional = prompt("Você tem nacionalidade brasileira? Digite true ou false:") === "true";
+
+if (idade16 && nacional){
+    alert("Você pode votar!")
+} else {
+    alert("Você não pode votar.")
+}*/
---
+++ Soluções Exercícios/aula04-pg/js/script.js
@@ -0,0 +1,69 @@
+/*let nota = Number(prompt("Informe uma Nota"))
+
+if (nota >=90){
+    alert("O conceito é A")
+} else if(nota >=80){
+    alert: ("O conceito é B")
+} else{
+    alert("O conceito é C")
+}*/
+
+/*let numero = Number(prompt("Informe um número"))
+
+switch(numero){
+    case 1 : alert("Domingo")
+      break;
+    case 2 : alert("Segunda")
+      break;
+    case 3 : alert("Terça")
+      break;
+    case 4 : alert("Quarta")
+      break;
+    case 5 : alert("Quinta")
+      break;
+    case 6 : alert("Sexta")
+      break;
+    case 7 : alert("Sábado")
+      break;
+
+    default:
+    alert("Dia inválido")
+}*/
+
+/*let temperatura = Number(prompt("Informe a temperatura:"))
+
+let temp = temperatura > 25 ? "Quente" : "Frio";
+alert(temp)*/
+
+/*let maiorIdade = True
+
+let carteira = prompt(
+    "Tem carteira? Digite true ou false:"
+) === "true";
+
+if (maiorIdade && carteira){
+    alert("Pode dirigir!")
+} else {
+    alert("Não pode dirigir.")
+}*/
+
+/*let numero = Number(prompt("Informe um número."))
+
+if (numero >=10 && numero <21){
+    alert("Dentro do intervalo")
+} else {
+    alert("Fora do intervalo!")
+} */
+
+/*let semaforo = prompt("Informe a cor do semáforo").toLowerCase()
+
+switch(semaforo) {
+    case "vermelho" : alert("Pare!")
+    break;
+    case "verde" : alert("Siga avante")
+    break;
+    case "amarelo" : alert("Atenção")
+    break;
+
+    default: alert("Cor indefinida.")
+}*/
