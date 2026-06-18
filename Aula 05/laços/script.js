f02f43b75ae341e20827eaa41323423a4b25f23f
Gregory <gregorysantos575@gmail.com>
2026-06-18 16:52:41 -0300

    exercicios 18/06 js

--- Aula 05/laços/script.js
+++ Aula 05/laços/script.js
@@ -1,10 +1,80 @@
-let soma = 0 
+// let soma = 0 
 
-for( let i = 0; i <= 15; i++){
+// for( let i = 0; i <= 15; i++){
 
-if (i % 2 == 0){
-    continue
-} else {
-    console.log("Número ímpar:" + i)
-}
+// if (i % 2 == 0){
+//     continue
+// } else {
+//     console.log("Número ímpar:" + i)
+// }
+// } 
+
+// for(let num1 = 1; num1 <= 10; num1++){
+//     console.log(num1)
+// }
+
+// for(let pares = 0; pares <=20; pares++)
+//     if (pares% 2 == 0){
+//         console.log(pares)
+//     }
+
+// for(let contagem = 5; contagem >= 0; contagem--){
+//     console.log(contagem)
+// }console.log("🚀 LANÇAMENTO!")
+
+// let numero = 7;
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(numero + " x " + i + " = " + (numero * i));
+// } 
+//
+// let bateria = 100;
+
+// while(bateria > 0){
+//     console.log("Bateria: " + bateria + "%")
+//     bateria -= 20;
+// }
+
+// let senha = 123;
+// while(senha !== "123"){
+//     senha=prompt("Informe a senha")
+//     if(senha !== "123"){
+//         alert("Senha incorreta. Tente novamente.")
+//     }
+// } alert("Acesso liberado!")
+
+// let energia = 100
+
+// while(energia >= 0){
+//     console.log("Energia: " + energia)
+//     energia -= 10;
+// }
+
+// do {
+//     console.log("Tentando jogar...")
+// } while (vereito > 0)
+
+// let opcao
+
+// do{
+//     opcao = Number(prompt("Digite..."))
+//     switch(opcao){
+//     case 1: alert("Jogando")
+//     break;
+//     case 2: alert("Ranking")
+//     break;
+//     case 3: alert("Sair")
+//     break;
+//     default: alert("Opção inválida.")
+// }
+// } while(opcao !== 3);
+
+let playlist = [
+    "me ama",
+    "escudo",
+    "quem é esse?"
+]
+
+for(let i = 0; i < playlist.length; i++){
+console.log(playlist[2])
 }
